03946460a9dec235bd56fa4aa909055e3f642d02
Tobias Hafner <t.hafner@stud.unibas.ch>
2022-01-21 07:07:09 +0100

    improve bounce

--- src/model/Animation.cpp
+++ src/model/Animation.cpp
@@ -7,37 +7,25 @@
 // include header for Animation
 #include "Animation.h"
 
-// contructor
-Animation::Animation() {
-}
-
 //.............................................
-//BEGIN: VARIABLES USED BY THIS ANIMATION...
+//BEGIN: Variables used by this animation...
 //.............................................
 
-float x_pos = 0;
-float y_pos = 1;
-float z_pos = 2;
+int x_pos = 0;
+int y_pos = 1;
+int z_pos = 2;
 
-float x_change = 1;
-float y_change = 1;
-float z_change = 1;
+int x_change = 1;
+int y_change = 1;
+int z_change = 1;
 
 byte color = 0;
 byte colors[] = {0b000001, 0b000100, 0b000101, 0b010000, 0b010001, 0b010100, 0b010101};
 
 //...........................................
-//END: VARIABLES USED BY THIS ANIMATION...
+//END: Variables used by this animation...
 //...........................................
 
-// assigns a cube to this animation
-void Animation::assignCube(Cube* cube){
-  _cube = cube;
-}
-// assigns a writer to this animation
-void Animation::assignWriter(Writer* writer) {
-  _writer = writer;
-}
 // calculates the next frame and updates the cube object
 void Animation::renderNextFrame() {
     // check if calculation of next frame is already necessary. Else skip to refreshing current frame for less flicker
@@ -46,7 +34,7 @@ void Animation::renderNextFrame() {
     }
    
     //...................................
-    //FRAME CALCULATION STARTS HERE...
+    //BEGIN: Frame calculations...
     //...................................
     
     // turn of previous led
@@ -59,22 +47,62 @@ void Animation::renderNextFrame() {
     _cube->setLed(round(x_pos), round(x_pos), round(x_pos), colors[color]);
     //update direction
     if (x_pos < 1 or x_pos > 4) {
-      x_change = -x_change + randomDeviation();
+      x_change = newDirection(x_change);
       color = (color++ % 7);
     }
     if (y_pos < 1 or y_pos > 4){
-      y_change = -y_change + randomDeviation();
+      y_change = newDirection(y_change);
       color = (color++ % 7);
     }
     if (z_pos < 1 or z_pos > 4){
-      z_change = -z_change + randomDeviation();
+      z_change = newDirection(z_change);
       color = (color++ % 7);
     }
     
     //...................................
-    //FRAME CALCULATION ENDS HERE...
+    //END: Frame calculations...
     //...................................
 }
+
+//.............................................
+//BEGIN: Helper methods for animation...
+//.............................................
+// creates a random number from -0.4 to 0.4
+int Animation::newDirection(int oldDirection) {
+  int r = random(2) + 1;
+  if (oldDirection > 0) {
+    r *= -1;
+  }
+  return r;
+}
+
+// maps positions outside of the cube to the nearest pixel
+int Animation::limit(int coord) {
+  if (coord < 0) {
+    return 0;
+  }
+  if (coord > 5) {
+    return 5;
+  }
+  return coord; 
+}
+//.............................................
+//END: Helper methods for animation...
+//.............................................
+
+// contructor
+Animation::Animation() {
+}
+
+// assigns a cube to this animation
+void Animation::assignCube(Cube* cube){
+  _cube = cube;
+}
+// assigns a writer to this animation
+void Animation::assignWriter(Writer* writer) {
+  _writer = writer;
+}
+
 // writes the next frame to the hardware
 void Animation::showNextFrame() {
   // check if rewriting of current frame is already necessary. Minimizes jitter
@@ -82,11 +110,6 @@ void Animation::showNextFrame() {
       return;
   }
   _writer->writeCube();
-}
-
-// creates a random number from -0.4 to 0.4
-float Animation::randomDeviation() {
-  return (random(9) - 4) / 10.0;
 }
 
 // checks if the current frame is to early
--- src/model/Animation.h
+++ src/model/Animation.h
@@ -34,12 +34,14 @@ class Animation {
         // refresh rate of cube >>> D O N ' T  T O U C H! <<<
         const float _refreshDelta = 0;
         // framerate of the animation
-        const float _frameDelta = 1000 / 60;
+        const float _frameDelta = 1000 / 2;
         // checks if frame is to early
         bool frameIsToEarly();
         // checks if frame is to early
         bool refreshIsToEarly();
-        // creates a random number from -0.4 to 0.4
-        float randomDeviation();
-        };
+        // calculates the new direciton for the bounce
+        int newDirection(int oldDirection);
+        // maps positions outside of the cube to the nearest pixel
+        int limit(int coord);
+};
 #endif
