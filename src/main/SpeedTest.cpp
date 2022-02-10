817eb9b5f30107c5eec8bf12e888aa881c3c6d8a
Tobias Weber <tm.weber@stud.unibas.ch>
2022-02-10 16:02:00 +0100

    Improved SpeedTest

--- src/main/SpeedTest.cpp
+++ src/main/SpeedTest.cpp
@@ -15,19 +15,62 @@ void SpeedTest::renderNextFrame() {
     // check if calculation of next frame is already necessary. Else skip to refreshing current frame for less flicker
     if (frameIsToEarly()) {
       return;
-    }    
-    
-    _cube->setLed(index, 0);
-    color++;
-    if (color == 7) {
-      color = 0;
-      if (index == 124) {
-        index = 0;
-      } else {
-        index++;
-      }
     }
-    _cube->setLed(index, colors[color]);
+
+    if (state == 0) {
+      state++;
+      startTime = millis();
+
+    } else if (state == 1) {
+      if (millis() - startTime < runTime) {
+        _cube->setLed(index, 0);
+        color++;
+        if (color == 7) {
+          color = 0;
+          if (index == 124) {
+            index = 0;
+          } else {
+            index++;
+          }
+        }
+        _cube->setLed(index, colors[color]);
+        counter++;
+      } else {
+        state++;
+        int framerate = counter * 1000 / runTime;
+        digits[3] = framerate % 10;
+        framerate /= 10;
+        digits[2] = framerate % 10;
+        framerate /= 10;
+        digits[1] = framerate % 10;
+        framerate /= 10;
+        digits[0] = framerate % 10;
+        startTime = millis();
+      }
+
+    } else if (state == 2) {
+      _cube->clearLeds();
+      if (millis() - startTime < displayTime) {
+        byte* plane = _charLib->getColoredCharacter(digits[0] + 26, 0b110000);
+        _cube->setPlane(0, 4, plane);
+        free(plane);
+      } else if (millis() - startTime < 2 * displayTime) {
+        byte* plane = _charLib->getColoredCharacter(digits[1] + 26, 0b001100);
+        _cube->setPlane(0, 4, plane);
+        free(plane);
+      } else if (millis() - startTime < 3 * displayTime) {
+        byte* plane = _charLib->getColoredCharacter(digits[2] + 26, 0b000011);
+        _cube->setPlane(0, 4, plane);
+        free(plane);
+      } else if (millis() - startTime < 4 * displayTime) {
+        byte* plane = _charLib->getColoredCharacter(digits[3] + 26, 0b111100);
+        _cube->setPlane(0, 4, plane);
+        free(plane);
+      } else if (millis() - startTime > 6 * displayTime) {
+        startTime = millis();
+      }
+
+    }
 }
 
 // C O N S T R U C T O R
@@ -37,6 +80,6 @@ SpeedTest::SpeedTest() {
   // or initialize it in the header file
 
   // set your Framerate here
-  int fps = 2000; 
+  int fps = 2000; // everything above 1000 yields the same result
   _frameDelta = 1000 / fps;
 }
--- src/main/SpeedTest.h
+++ src/main/SpeedTest.h
@@ -10,6 +10,12 @@ class SpeedTest:public Animation {
         byte color = 0;
         byte index = 0;
         byte colors[7] = {0b000001, 0b010000, 0b000100, 0b010100, 0b010001, 0b000101, 0b010101};
+        long counter = 0;
+        byte state = 0;
+        int runTime = 2000; // in millis
+        unsigned long startTime;
+        byte digits[4] = {0,0,0,0};
+        int displayTime = 1000; // time per digit in millis
     public:
         void renderNextFrame();
         SpeedTest();
